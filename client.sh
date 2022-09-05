0969c141a98ddad82f9ba8737655ee0f958f8659
kikito <nikita.tyunyayev@student.uclouvain.be>
2022-09-05 10:45:07 +0000

    added client and server scripts

---
+++ client.sh
@@ -0,0 +1,1 @@
+sudo perf record --call-graph dwarf -o /home/nikita/memoire/dpdk_picoquic/perfMeasures/perf_quiche_client.data target/release/examples/http3-client -G 10000000000 -X keys.log 10.100.0.2 4445
---
+++ server.sh
@@ -0,0 +1,1 @@
+sudo perf record --call-graph dwarf -o /home/nikita/memoire/dpdk_picoquic/perfMeasures/perf_quiche_server.data target/release/examples/http3-server -p 4445 -k cert.key -c cert.crt
